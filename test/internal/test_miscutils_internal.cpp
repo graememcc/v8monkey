beedbf503d7ab886ae470a00e5498ea0f8e765d8
Graeme McCutcheon <graememcc_firefox@graeme-online.co.uk>
2015-02-10 21:49:13 +0000

    Init style test/internal/test_miscutils_internal.cpp

--- test/internal/test_miscutils_internal.cpp
+++ test/internal/test_miscutils_internal.cpp
@@ -8,28 +8,28 @@
 #include "V8MonkeyTest.h"
 
 
-namespace MiscUtils = v8::V8Monkey::MiscUtils;
+using v8::V8Monkey::MiscUtils::TwoByteLength;
 
 
 V8MONKEY_TEST(IntTwoByteLength001, "Empty string has length 0") {
-  const uint16_t data[] = {0x0000};
-  V8MONKEY_CHECK(MiscUtils::TwoByteLength(&data[0]) == 0, "Length is correct");
+  const uint16_t data[] {0x0000};
+  V8MONKEY_CHECK(TwoByteLength(&data[0]) == 0, "Length is correct");
 }
 
 
 V8MONKEY_TEST(IntTwoByteLength002, "Non-empty string has correct length (1)") {
-  const uint16_t data[] = {0x0041, 0x0000};
-  V8MONKEY_CHECK(MiscUtils::TwoByteLength(&data[0]) == 1, "Length is correct");
+  const uint16_t data[] {0x0041, 0x0000};
+  V8MONKEY_CHECK(TwoByteLength(&data[0]) == 1, "Length is correct");
 }
 
 
 V8MONKEY_TEST(IntTwoByteLength003, "Non-empty string has correct length (2)") {
-  const uint16_t data[] = {0x0041, 0x0060, 0x0000};
-  V8MONKEY_CHECK(MiscUtils::TwoByteLength(&data[0]) == 2, "Length is correct");
+  const uint16_t data[] {0x0041, 0x0060, 0x0000};
+  V8MONKEY_CHECK(TwoByteLength(&data[0]) == 2, "Length is correct");
 }
 
 
 V8MONKEY_TEST(IntTwoByteLength004, "Non-empty string has correct length (3)") {
-  const uint16_t data[] = {0x0041, 0x0060, 0x0000, 0x0000};
-  V8MONKEY_CHECK(MiscUtils::TwoByteLength(&data[0]) == 2, "Length is correct");
+  const uint16_t data[] {0x0041, 0x0060, 0x0000, 0x0000};
+  V8MONKEY_CHECK(TwoByteLength(&data[0]) == 2, "Length is correct");
 }
