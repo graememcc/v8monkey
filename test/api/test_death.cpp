acdd239ec59e80135eb5fa387e30429d087f4478
Graeme McCutcheon <graememcc_firefox@graeme-online.co.uk>
2015-02-12 16:23:52 +0000

    Fix crash in test death: must enter isolate

--- test/api/test_death.cpp
+++ test/api/test_death.cpp
@@ -18,7 +18,11 @@ V8MONKEY_TEST(Death001, "V8::IsDead normally returns false (no init)") {
 
 
 V8MONKEY_TEST(Death002, "V8::IsDead normally returns false (with init)") {
+  Isolate* i {Isolate::New()};
   V8::Initialize();
+  i->Enter();
   V8MONKEY_CHECK(!V8::IsDead(), "I ain't dead yet!");
+  i->Exit();
+  i->Dispose();
   V8::Dispose();
 }
