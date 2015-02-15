2e269bb9f88b70b5ffee8fbb5d89d4f37a88b57e
Graeme McCutcheon <graememcc_firefox@graeme-online.co.uk>
2015-02-15 14:21:24 +0000

    New SpiderMonkey stand up logic

--- src/utils/SpiderMonkeyUtils.cpp
+++ src/utils/SpiderMonkeyUtils.cpp
@@ -1,3 +1,12 @@
+// atomic_int
+#include <atomic>
+
+// JS_Init, JS_ShutDown
+#include "jsapi.h"
+
+// unique_ptr
+#include <memory>
+
 // TLSKey, CreateTLSKey, GetTLSData, StoreTLSData
 // XXX Remove me if we don't need this for compartments
 #include "platform/platform.h"
@@ -5,13 +14,92 @@
 // SpiderMonkeyUtils definition
 #include "utils/SpiderMonkeyUtils.h"
 
+// V8_UNUSED
+#include "v8config.h"
+
+// TriggerFatalError
+#include "V8MonkeyCommon.h"
+
 
 // XXX Remove me if we don't need this for compartments
 using namespace v8::V8Platform;
 
 
+namespace {
+  class SpiderMonkeyTearDown;
+
+
+  std::unique_ptr<SpiderMonkeyTearDown> tearDown {nullptr};
+
+  std::atomic<int> runtimeCount {0};
+
+  /*
+   * This class is used to ensure we always teardown SpiderMonkey after it has been initialized, regardless of whether
+   * the client remembers to call V8::Dispose or not.
+   *
+   * The game here is that we have a unique_ptr tearDown with static storage duration. When SpiderMonkey is
+   * initialized, we heap allocate a new instance of SpiderMonkeyTearDown, and give it to the unique_ptr to manage the
+   * lifetime. Then, all we need is for the destructor to check to see if SM has been correctly shutdown, and if not
+   * go ahead and invoke the shutdown.
+   *
+   */
+
+  class SpiderMonkeyTearDown {
+    public:
+      SpiderMonkeyTearDown() : isDisposed {false} {}
+
+      ~SpiderMonkeyTearDown() {
+        if (!isDisposed) {
+          AttemptDispose(true);
+        }
+      }
+
+      void AttemptDispose(bool isBeingDestroyed = false) {
+        if (isBeingDestroyed) {
+          int threadsWithRuntimes {std::atomic_load(&runtimeCount)};
+          V8MONKEY_ASSERT(threadsWithRuntimes <= 1, "Attempting to destroy V8 when other threads still exist");
+
+          // XXX Add logic to force disposal of last thread's runtime
+          if (threadsWithRuntimes == 1) {
+          }
+        }
+
+        JS_ShutDown();
+        isDisposed = true;
+      }
+
+      SpiderMonkeyTearDown(const SpiderMonkeyTearDown& other) = delete;
+      SpiderMonkeyTearDown(SpiderMonkeyTearDown&& other) = delete;
+      SpiderMonkeyTearDown& operator=(const SpiderMonkeyTearDown& other) = delete;
+      SpiderMonkeyTearDown& operator=(SpiderMonkeyTearDown&& other) = delete;
+
+    private:
+      bool isDisposed {false};
+  };
+}
+
+
 namespace v8{
-  namespace V8Monkey {
+  namespace SpiderMonkey {
+    void EnsureSpiderMonkey() {
+      static bool V8_UNUSED initialized {[]() {
+        bool successful {JS_Init()};
+
+        if (!successful) {
+          // I doubt we can do anything useful if we failed to init SpiderMonkey (assumes TriggerFatalError aborts)
+          ::v8::V8Monkey::TriggerFatalError("v8::Initialize", "Failed to init Spidermonkey", false);
+          // Unreached
+          return false;
+        }
+
+        SpiderMonkeyTearDown* smTearDown {new SpiderMonkeyTearDown};
+        tearDown.reset(smTearDown);
+        return true;
+      }()};
+    }
+
+
+/*
     JSRuntime* SpiderMonkeyUtils::GetJSRuntimeForThread() {
       RTCXData rtcx = GetJSRuntimeAndJSContext();
       return rtcx.rt;
@@ -34,5 +122,6 @@ namespace v8{
       // XXX TODO
       return nullptr;
     }
+*/
   }
 }
